57c9f384c84b16ae3cd427d32bc0cd85e5fe3a15
Juhos István <istvanj.dev@gmail.com>
2018-03-19 01:01:52 +0100

    Clean up swagger generated code

--- app/src/main/java/hu/bme/aut/stewe/rebrickableclient/network/swagger/api/UsersApi.java
+++ app/src/main/java/hu/bme/aut/stewe/rebrickableclient/network/swagger/api/UsersApi.java
@@ -1,18 +1,15 @@
 package hu.bme.aut.stewe.rebrickableclient.network.swagger.api;
 
-import io.swagger.client.CollectionFormats.*;
-
-
 import retrofit2.Call;
-import retrofit2.http.*;
-
-import okhttp3.RequestBody;
-
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import retrofit2.http.DELETE;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.PATCH;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface UsersApi {
   
