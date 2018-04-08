064cf1b5501335ab73de79f0fe3b41f8890e0af9
Juhos István <istvanj.dev@gmail.com>
2018-04-08 02:00:56 +0200

    Add necessary providings to UIModule

--- app/src/main/java/hu/bme/aut/stewe/rebrickableclient/ui/UIModule.kt
+++ app/src/main/java/hu/bme/aut/stewe/rebrickableclient/ui/UIModule.kt
@@ -3,8 +3,14 @@ package hu.bme.aut.stewe.rebrickableclient.ui
 import android.content.Context
 import dagger.Module
 import dagger.Provides
+import hu.bme.aut.stewe.rebrickableclient.interactor.LoginInteractor
+import hu.bme.aut.stewe.rebrickableclient.interactor.SetDetailsInteractor
+import hu.bme.aut.stewe.rebrickableclient.interactor.SetListsInteractor
+import hu.bme.aut.stewe.rebrickableclient.interactor.SetsInteractor
 import hu.bme.aut.stewe.rebrickableclient.ui.login.LoginPresenter
 import hu.bme.aut.stewe.rebrickableclient.ui.setdetails.SetDetailsPresenter
+import hu.bme.aut.stewe.rebrickableclient.ui.setlists.SetListsPresenter
+import hu.bme.aut.stewe.rebrickableclient.ui.sets.SetsPresenter
 import javax.inject.Singleton
 
 @Module
@@ -19,5 +25,31 @@ class UIModule(var context: Context) {
 
     @Provides
     @Singleton
+    fun provideLoginInteractor() = LoginInteractor()
+
+    @Provides
+    @Singleton
     fun provideSetDetailsPresenter() = SetDetailsPresenter()
+
+    @Provides
+    @Singleton
+    fun provideSetDetailsInteractor() = SetDetailsInteractor()
+
+    @Provides
+    @Singleton
+    fun provideSetListsPresenter() = SetListsPresenter()
+
+    @Provides
+    @Singleton
+    fun provideSetListssInteractor() = SetListsInteractor()
+
+    @Provides
+    @Singleton
+    fun provideSetsPresenter() = SetsPresenter()
+
+    @Provides
+    @Singleton
+    fun provideSetsDetailsInteractor() = SetsInteractor()
+
+
 }
