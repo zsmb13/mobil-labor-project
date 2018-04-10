2d4df8d2b4173741cfa744f7cf892003e53edce1
Juhos István <istvanj.dev@gmail.com>
2018-04-10 09:25:34 +0200

    Create InteractorModule

---
+++ app/src/main/java/hu/bme/aut/stewe/rebrickableclient/interactor/InteractorModule.kt
@@ -0,0 +1,24 @@
+package hu.bme.aut.stewe.rebrickableclient.interactor
+
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class InteractorModule {
+    @Provides
+    @Singleton
+    fun provideLoginInteractor() = LoginInteractor()
+
+    @Provides
+    @Singleton
+    fun provideSetDetailsInteractor() = SetDetailsInteractor()
+
+    @Provides
+    @Singleton
+    fun provideSetListsInteractor() = SetListsInteractor()
+
+    @Provides
+    @Singleton
+    fun provideSetsInteractor() = SetsInteractor()
+}
