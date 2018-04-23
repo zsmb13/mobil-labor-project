f8c8c0747e9387711700564872080879cbf58638
Juhos István <istvanj.dev@gmail.com>
2018-04-23 18:08:58 +0200

    Add ViewGroup.infate extension to UIExtensions

--- app/src/main/java/hu/bme/aut/stewe/rebrickableclient/ui/UIExtensions.kt
+++ app/src/main/java/hu/bme/aut/stewe/rebrickableclient/ui/UIExtensions.kt
@@ -1,7 +1,11 @@
 package hu.bme.aut.stewe.rebrickableclient.ui
 
+import android.content.Context
+import android.support.annotation.LayoutRes
 import android.support.design.widget.Snackbar
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 
 
 fun View.longSnack(msg: String) =
@@ -9,3 +13,6 @@ fun View.longSnack(msg: String) =
 
 fun View.shortSnack(msg: String) =
         Snackbar.make(this, msg, Snackbar.LENGTH_SHORT)
+
+fun ViewGroup.inflate(context: Context, @LayoutRes layoutId: Int) =
+        LayoutInflater.from(context).inflate(layoutId, this, false)
