04d13aaf158845e2777095f3649881e59d14a786
Juhos István <istvanj.dev@gmail.com>
2018-04-26 09:14:18 +0200

    Implement default showMessage() method in BaseActivity

--- app/src/main/java/hu/bme/aut/stewe/rebrickableclient/ui/BaseActivity.kt
+++ app/src/main/java/hu/bme/aut/stewe/rebrickableclient/ui/BaseActivity.kt
@@ -1,11 +1,12 @@
 package hu.bme.aut.stewe.rebrickableclient.ui
 
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 
 
-abstract class BaseActivity : AppCompatActivity(){
-    fun showMessage(msg:String){
-        // TODO default message display method
+abstract class BaseActivity : AppCompatActivity() {
+    fun showMessage(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
     }
 }
 
