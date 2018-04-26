963a12da9a28384826f53cf2c19c4efb83ad7d99
Juhos István <istvanj.dev@gmail.com>
2018-04-26 10:02:30 +0200

    Add Glide image loading extension to UIExtensions

--- app/src/main/java/hu/bme/aut/stewe/rebrickableclient/ui/UIExtensions.kt
+++ app/src/main/java/hu/bme/aut/stewe/rebrickableclient/ui/UIExtensions.kt
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 
 fun View.longSnack(msg: String) =
@@ -16,3 +18,6 @@ fun View.shortSnack(msg: String) =
 
 fun ViewGroup.inflate(context: Context, @LayoutRes layoutId: Int) =
         LayoutInflater.from(context).inflate(layoutId, this, false)!!
+
+fun ImageView.loadFromUrl(url: String?) =
+        Glide.with(context).load(url).into(this)
